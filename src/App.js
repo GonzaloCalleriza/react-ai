660ab5c20b95cfa48d78da37fcc66059c89bc394
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-19 22:36:07 +0100

    Finished App front end

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, {useEffect, useState} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
 
 const alanKey = process.env.REACT_ALAN_AI_KEY;
 
 const App = () => {
+
+    const classes = useStyles();
 
     const [newsArticles, setNewsArticles] = useState([]);
 
@@ -23,6 +26,9 @@ const App = () => {
 
     return(
         <div>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanlogo} alt='alan logo'/>
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     )
