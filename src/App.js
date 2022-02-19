f631efa592a33c4ce93ffd15389555e9eb234a43
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-19 13:51:48 +0100

    Managed connection with Alan AI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
 const App = () => {
+
+    const alanKey = process.env.REACT_ALAN_AI_KEY;
+
+    useEffect(() =>{
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command}) => {
+                if(command === 'testCommand') {
+                    alert('This code was executed');
+                }
+            }
+        })
+    }, [])
+
     return(
         <div>
             React Alan AI News Application
